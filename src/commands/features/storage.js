125b9e695e361a0d57cec16c7012f16872efc153
Wixonic <public.wixonic@icloud.com>
2024-01-18 14:52:25 +0100

    Update

--- src/commands/features/storage.js
+++ src/commands/features/storage.js
@@ -3,7 +3,22 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("storage")
-		.setDescription("Allows to share big files"),
+		.setDescription("Allows to share big files")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("upload")
+				.setDescription("Upload a big file"))
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("download")
+				.setDescription("Download a big file")
+				.addStringOption((option) =>
+					option
+						.setName("upload-id")
+						.setDescription("The upload ID of the file")
+						.setRequired(true)
+				)
+		),
 
 	async execute(interaction) {
 
