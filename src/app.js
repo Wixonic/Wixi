b7526b09622e3c5ba0f4f184ea6e4f0ebbde33c3
Wixonic <public.wixonic@icloud.com>
2024-02-14 23:07:24 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-const { } = require("electron");
+const { app, Menu, nativeImage, Tray } = require("electron");
+const path = require("path");
+
+app.once("ready", () => {
+	const tray = new Tray(path.join(__dirname, "assets/tray.png"));
+
+	tray.setContextMenu(Menu.buildFromTemplate([
+		{
+			label: "Quit",
+			type: "normal",
+			click: () => app.quit()
+		}
+	]));
+
+	tray.setToolTip(app.getName());
+
+	app.dock.hide();
+});
