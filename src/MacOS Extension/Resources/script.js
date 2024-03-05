f54ae79737540961d5d12cd0bad383c91e626533
Wixonic <nicolas.lablee@icloud.com>
2024-03-05 23:03:37 +0100

    Add basic support for github.com
    Add support for github.com/profile and github.com/owner/repository

--- src/MacOS Extension/Resources/script.js
+++ src/MacOS Extension/Resources/script.js
@@ -1,3 +1,69 @@
+const extensions = [
+    {
+        matches: [
+            /^https:\/\/github\.com\/([\w-]+)\/([\w-]+)/
+        ],
+        run: (url, owner, repo) => {
+            sendStatus("POST", "/github", {
+                type: "repository",
+                owner,
+                repository: repo,
+                url: `https://github.com/${owner}/${repo}`
+            });
+            
+            onUnload.path = "/github";
+            onUnload.data = {
+                type: "repository",
+                owner,
+                repository: repo,
+            };
+            
+            console.log([owner, repo]);
+        }
+    }, {
+        matches: [
+            /^https:\/\/github\.com\/([\w-]+)\/
+        ],
+        run: (url, profile) => {
+            sendStatus("POST", "/github", {
+                type: "profile",
+                profile,
+                url: `https://github.com/${profile}`
+            });
+            
+            onUnload.path = "/github";
+            onUnload.data = {
+                type: "profile",
+                profile
+            };
+        }
+    }
+];
+
+const sendStatus = (method="POST", path="/", data={}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, new URL(path, "https://server.wixonic.fr:4000"));
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+};
+
+const onUnload = {
+    data: null,
+    path: null
+};
+
+window.addEventListener("unload", () => sendStatus("DELETE", onUnload.path, onUnload.data));
 window.addEventListener("load", () => {
-    alert(window.location.href);
+    const url = location.href;
+    
+    for (const extension of extensions) {
+        for (const match of extension.matches) {
+            const results = match.exec(url);
+			
+            if (results.length > 0) {
+                extension.run(...results);
+                return;
+            }
+        }
+    }
 });
