0f768ac44848c0f73eb65a32ec8f0013921b6156
Wixonic <public.wixonic@icloud.com>
2024-03-19 19:19:07 +0100

    Created HTTP Server

--- src/Plugins/http_server/plugin.js
+++ src/Plugins/http_server/plugin.js
@@ -10,8 +10,20 @@ const app = express();
 
 const log = (txt) => console.log(`[${package.displayName ?? package.name}]: ${txt}`);
 
-app.use((req, res, next) => {
+app.use((req, _, next) => {
 	log(`Incomming request from ${req.socket.remoteAddress ?? "unknown ip"} - ${path.join(req.headers.host, req.url)}`);
+	next();
+});
+
+app.use((req, res, next) => {
+	const url = new URL(req.url, `https://${req.headers.host}`);
+
+	res.writeHead(301, {
+		"location": url.href
+	}).end();
+
+	log(`Redirected to: ${url.href}`);
+
 	next();
 });
 
