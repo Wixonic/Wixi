b742e6430f8c85532e466d08beb5b0fd19b2d984
Wixonic <public.wixonic@icloud.com>
2024-05-21 13:59:35 +0200

    Remove BrawlData

--- src/Plugins/rpc_server/extensions/roblox.js
+++ src/Plugins/rpc_server/extensions/roblox.js
@@ -36,10 +36,6 @@ const cycle = async () => {
 					small_text: "Roblox"
 				},
 
-				timestamps: {
-					start: new Date(presence.lastOnline).getTime()
-				},
-
 				buttons: [
 					"Play",
 					"Open my profile"
@@ -63,10 +59,6 @@ const cycle = async () => {
 				assets: {
 					small_image: config.assets.logo_roblox_studio,
 					small_text: "Roblox Studio"
-				},
-
-				timestamps: {
-					start: new Date(presence.lastOnline).getTime()
 				},
 
 				buttons: [
